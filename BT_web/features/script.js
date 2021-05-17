d903c20a8f9ad8b743cea4f37cad4b02ef5a2ab4
VTFT <max.traineau@yahoo.fr>
2021-05-17 09:37:26 +0200

    Delete BT_web directory

--- BT_web/features/script.js
+++
@@ -1,9 +0,0 @@
-    // fcts for borgar
-function borgar(){
-    document.getElementById("borgarMenu").style.display = "flex";
-}
-
-function noBorgar(){
-    document.getElementById("borgarMenu").style.display = "none";
-}
-    // fcts for borgar end
