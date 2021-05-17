fe34e6a7783aaa10b4876fe3e1bdd6e2606da69a
VTFT <max.traineau@yahoo.fr>
2021-05-17 09:39:14 +0200

    Add files via upload

---
+++ community/script.js
@@ -0,0 +1,17 @@
+    // fcts for borgar
+function borgar(){
+    document.getElementById("borgarMenu").style.display = "flex";
+}
+
+function noBorgar(){
+    document.getElementById("borgarMenu").style.display = "none";
+}
+    // fcts for borgar end
+
+function showForm(){
+    document.getElementById("form").style.display = "flex";
+}
+
+function hideForm(){
+    document.getElementById("form").style.display = "none";
+}
---
+++ invite/script.js
@@ -0,0 +1,13 @@
+    // fcts for borgar
+function borgar(){
+    document.getElementById("borgarMenu").style.display = "flex";
+}
+
+function noBorgar(){
+    document.getElementById("borgarMenu").style.display = "none";
+}
+    // fcts for borgar end
+
+function showMe(){
+    document.getElementById("arrow").style.display = "block";
+}
